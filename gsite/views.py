2281e0850866da2a62fd094935d1b0b5bbd0fa43
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2025-07-29 10:25:15 -0400

    Add landing page

--- gsite/views.py
+++ gsite/views.py
@@ -2,6 +2,9 @@ from django.conf import settings
 from django.shortcuts import render
 
 def index(request):
+    if request.headers.get('HX-Request'):  
+        return render(request, 'gsite/_home.html')
+
     return render(
         request, "gsite/index.html",
         )

481491cffd0a2cae8c5620d8e4a82275b41aac60
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2025-07-29 11:24:31 -0400

    Move navbar into partial

--- gsite/views.py
+++ gsite/views.py
@@ -2,24 +2,32 @@ from django.conf import settings
 from django.shortcuts import render
 
 def index(request):
+    context = {'active_tab': 'home'}
+
     if request.headers.get('HX-Request'):  
-        return render(request, 'gsite/_home.html')
+        return render(request, 'gsite/_home.html', context)
 
     return render(
-        request, "gsite/index.html",
+        request, "gsite/index.html", context
         )
 
 def cli(request):
+    context = {'active_tab': 'cli'}
+
     return render(
-        request, "gsite/_cli.html",
+        request, "gsite/_cli.html", context
         )
 
 def web(request):
+    context = {'active_tab': 'web'}
+
     return render(
-        request, "gsite/_web.html",
+        request, "gsite/_web.html", context,
         )
 
 def blog(request):
+    context = {'active_tab': 'blog'}
+
     return render(
-        request, "gsite/_blog.html",
+        request, "gsite/_blog.html", context,
         )

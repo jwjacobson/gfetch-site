b9aa8962cbb7989c82da96dbd1f0ee4a4c020811
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2025-08-04 15:43:19 -0400

    Fix body declaration

--- gsite/models.py
+++ gsite/models.py
@@ -3,7 +3,7 @@ from markdownx.models import MarkdownxField
 
 class BlogPost(models.Model):
     title = models.CharField(max_length=255)
-    body = models.MarkdownxField()
+    body = MarkdownxField()
     created_on = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
 

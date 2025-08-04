f348099b5a1bd12981da4e77bf8e4a0fa33e76f2
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2025-08-04 15:43:48 -0400

    Add missing comma to urls.py

--- project/urls.py
+++ project/urls.py
@@ -21,7 +21,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('gsite.urls'))
+    path('', include('gsite.urls')),
     path('markdownx/', include('markdownx.urls')),
 ]
 

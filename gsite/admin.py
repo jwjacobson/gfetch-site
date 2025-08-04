f0cfacfc041f5dfd9821562c5aae1e82b61a50a5
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2025-08-04 15:40:36 -0400

    Update admin page registration for BlogPost

--- gsite/admin.py
+++ gsite/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
+from markdownx.admin import MarkdownxModelAdmin
 
 from gsite.models import BlogPost
 
 @admin.register(BlogPost)
-class BlogPostAdmin(admin.ModelAdmin):
+class BlogPostAdmin(MarkdownxModelAdmin):
     list_display = ["title", "created_on"]
+    list_filter = ['created_on', 'updated_on']
     search_fields = ("title", "body", "created_on")
+    readonly_fields = ['created_on', 'updated_on']

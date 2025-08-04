818bbc88e7321ff0a210bfa42ceed9f9afc3996d
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2025-08-04 15:44:37 -0400

    Fix reference updated_on -> last_updated

--- gsite/admin.py
+++ gsite/admin.py
@@ -6,6 +6,6 @@ from gsite.models import BlogPost
 @admin.register(BlogPost)
 class BlogPostAdmin(MarkdownxModelAdmin):
     list_display = ["title", "created_on"]
-    list_filter = ['created_on', 'updated_on']
+    list_filter = ['created_on', 'last_updated']
     search_fields = ("title", "body", "created_on")
-    readonly_fields = ['created_on', 'updated_on']
+    readonly_fields = ['created_on', 'last_updated']

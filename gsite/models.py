7632cb0ba09271b67310a0973c7e594115eed11e
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2025-08-04 15:39:37 -0400

    Change BlogPost body field to Markdown

--- gsite/models.py
+++ gsite/models.py
@@ -1,15 +1,18 @@
 from django.db import models
-
+from markdownx.models import MarkdownxField
 
 class BlogPost(models.Model):
     title = models.CharField(max_length=255)
-    body = models.TextField()
+    body = models.MardownxField()
     created_on = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
     
+    class Meta:
+        ordering = ['-created_on']
+
     def get_paragraphs(self):
         paragraphs = self.body.split('\n\n')
         return [p.strip() for p in paragraphs if p.strip()]
